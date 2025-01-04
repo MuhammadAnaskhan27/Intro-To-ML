44a10f06f2f494182d23bc9dc05c359924b49a45
M.Anas Khan <professional2448824@Outlook.com>
2025-01-04 23:41:27 +0500

    Project

---
+++ Churn_Prediction_Project/Churn_Prediction.py
@@ -0,0 +1,8 @@
+import pandas as pd
+
+# Load the dataset
+url = "https://github.com/YBIFoundation/Dataset/raw/main/TelecomCustomerChurn.csv"
+churn_df = pd.read_csv(url)
+
+# Handle Missing Values
+churn_df.fillna(churn_df.median(),inplace=True)
